dc834201f3f740892032710403630f2ac27940a0
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-13 14:38:07 +0700

    proses pengurutan data pada array dari nilai kecil hingga nilai besar

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -33,4 +33,19 @@ void insertionsort()
     int j;
 
     for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i << ": ";
+        for (unt k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
 }
