bc1b929149d9c9825cc50016a4a62c35e13ca704
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-13 14:47:14 +0700

    memberi jarak

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -54,10 +54,10 @@ void display(){
     cout << endl;
     cout << "================="
     cout << "\nEelement Array" << endl;
-    cout << "=================";
+    cout << "================="; << endl;
 
     for(int j = 0; j < n; j++){
-        cout << arr{j} << endl;
+        cout << arr[j] << endl;
     }
     cout << endl;
 }
